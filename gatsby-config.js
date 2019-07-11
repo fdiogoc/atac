912eee77540d674a7b0cbf15ce7f2e84e5e10b22
Diogo Cardoso Fernandes <diogofernandes@outlook.com>
2019-07-11 02:43:45 -0300

    Algumas configurações

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,8 +2,8 @@ const postcssPresetEnv = require('postcss-preset-env')
 
 module.exports = {
   siteMetadata: {
-    title: 'Yelloecake',
-    siteUrl: 'https://yellowcake.netlify.com'
+    title: 'Atac',
+    siteUrl: 'https://atac.netlify.com'
   },
   plugins: [
     'gatsby-plugin-react-helmet',
